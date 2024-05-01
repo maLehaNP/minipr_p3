08d1997cb342dd94f4bc6ce29693bd9f34464228
maLeha <maLehaNP@users.noreply.github.com>
2024-05-01 18:53:07 +0400

    final

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ class MyWidget(QWidget, Ui_Form):
         # загрузка списка и таблицы
         with open('items.json') as items:
             self.loc_items = json.load(items)
-            print(self.loc_items)
             loc_items_l = len(self.loc_items)
             self.tableWidget.setRowCount(loc_items_l)
             self.tableWidget.setColumnCount(3)
             self.updtable()
             self.lcd.display(loc_items_l)
 
-        self.comboBox.addItems(self.loc_items.keys())
         self.cboxcng()
 
         # подключение кнопок
@@ -33,7 +31,7 @@ class MyWidget(QWidget, Ui_Form):
         self.down_btn.clicked.connect(self.buttons)
         self.comboBox.currentTextChanged.connect(self.cboxcng)
 
-    # функция кнопок
+    # функции кнопок
     def buttons(self):
         if self.sender() == self.yes_btn:
             self.yes_lbl.setText(f"{int(self.yes_lbl.text()) + 1}")
@@ -49,7 +47,6 @@ class MyWidget(QWidget, Ui_Form):
             else:
                 self.loc_items[q_text][1] = self.yes_lbl.text()
                 self.loc_items[q_text][2] = self.no_lbl.text()
-            print(self.loc_items)
             self.updtable()
         if self.sender() == self.reset_btn:
             self.yes_lbl.setText('0')
@@ -61,36 +58,45 @@ class MyWidget(QWidget, Ui_Form):
             self.no_lbl.setText(self.no_spin.text())
         if self.sender() == self.up_btn:
             crkey = self.tableWidget.item(self.tableWidget.currentRow(), 0).text()
-            self.loc_items[crkey][0] = self.loc_items[crkey][0] + 1
+            uppkey = self.tableWidget.item(self.tableWidget.currentRow() - 1, 0).text()
+            self.loc_items[crkey][0] = self.loc_items[crkey][0] - 1
+            self.loc_items[uppkey][0] = self.loc_items[uppkey][0] + 1
             self.updtable()
-            print(self.loc_items)
         if self.sender() == self.down_btn:
             crkey = self.tableWidget.item(self.tableWidget.currentRow(), 0).text()
-            self.loc_items[crkey][0] = self.loc_items[crkey][0] - 1
+            lowkey = self.tableWidget.item(self.tableWidget.currentRow() + 1, 0).text()
+            self.loc_items[crkey][0] = self.loc_items[crkey][0] + 1
+            self.loc_items[lowkey][0] = self.loc_items[lowkey][0] - 1
             self.updtable()
-            print(self.loc_items)
 
-    # рисует строку
-    def rowmaker(self, key):
+    # рисует строку по ключу
+    def drawrow(self, key):
         self.tableWidget.setItem(self.loc_items[key][0], 0, QTableWidgetItem(key))
         self.tableWidget.setItem(self.loc_items[key][0], 1, QTableWidgetItem(self.loc_items[key][1]))
         self.tableWidget.setItem(self.loc_items[key][0], 2, QTableWidgetItem(self.loc_items[key][2]))
 
+    # обновляет таблицу
     def updtable(self):
         for key in self.loc_items.keys():
-            self.rowmaker(key)
+            self.drawrow(key)
+        self.comboBox.clear()
+        self.comboBox.addItems(sorted(self.loc_items.keys(), key=lambda key: self.loc_items[key][0]))
 
+    # меняет все по комбо-боксу
     def cboxcng(self):
         curtext = self.comboBox.currentText()
-        self.q_lbl.setText(curtext)
-        self.yes_lbl.setText(self.loc_items[curtext][1])
-        self.no_lbl.setText(self.loc_items[curtext][2])
+        if curtext in self.loc_items.keys():
+            self.q_lbl.setText(curtext)
+            self.yes_lbl.setText(self.loc_items[curtext][1])
+            self.no_lbl.setText(self.loc_items[curtext][2])
 
+    # действие при закрытии
     def closeEvent(self, event):
         with open('items.json', 'w') as items:
             json.dump(self.loc_items, items)
         event.accept()
 
+    # декствие при нажатии клавишы
     def keyPressEvent(self, event):
         if self.q_line.hasFocus() and event.key() == 16777220:
             self.ok_btn.click()
